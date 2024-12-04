140164fb998b23ded32cd5f6a0464364d5f02f70
Maya <mayas2744@tuckahoeschools.org>
2024-12-04 13:32:24 +0000

    second commit

--- Loop Examples/doWhileExample.js
+++ Loop Examples/doWhileExample.js
@@ -1,1 +1,25 @@
+let outputDiv = document.getElementById("output");
+let passwordInput = document.getElementById("passwordInput");
+let submitButton = document.getElementById("submitButton");
 
+const correctPassword = "2744";
+outputDiv.innerHTML += "<h2>Using a password prompt</h2>";
+
+submitButton.addEventListener("click", () => {
+    let password;
+    let attempt = 0;
+
+    do{
+        password = passwordInput.value;
+
+        if(password === correctPassword){
+            outputDiv.innerHTML += "Access granted!<br>";
+            passwordInput.disabled = true;
+            submitButton.disabled = true;
+            break;
+        } else {
+            outputDiv.innerHTML += `Incorrect password. Attempt ${++attempt}. Try again.<br>`;
+            passwordInput.value = "";
+        }
+    } while(password !== correctPassword && !passwordInput.disabled)
+});
